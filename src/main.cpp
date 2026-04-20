54f530dccfc942b4754872fa58ef79fad1d3a667
ComaTV <comatvc@gmail.com>
2026-04-20 23:46:17 +0300

    io

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,34 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static void errorCallback(int error, const char* description) {
+    std::cerr << "GLFW Error [" << error << "]: " << description << std::endl;
+}
+
 int main() {
+    glfwSetErrorCallback(errorCallback);
+    
     if (!glfwInit()) {
         std::cout << "GLFW init failed!" << std::endl;
         return -1;
     }
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    std::cout << "GLFW initialized successfully" << std::endl;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    std::cout << "Creating window..." << std::endl;
     GLFWwindow* window = glfwCreateWindow(800, 600, "My 3D Game", NULL, NULL);
     if (!window) {
         std::cout << "Window creation failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
+    
+    std::cout << "Window created successfully" << std::endl;
 
     glfwMakeContextCurrent(window);
 
